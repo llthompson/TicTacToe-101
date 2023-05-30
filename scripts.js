0cfca6b5330dcc4db3966f49838e351a00770f2a
llthompson <lisalacythompson+github@gmail.com>
2023-05-29 19:42:02 -0500

    mostly done

--- scripts.js
+++ scripts.js
@@ -22,7 +22,6 @@ let board = [
 
 const handleClick = (element) => {
 
-  
 
 
   console.log(`The element you clicked on has an id:  ${element.id}`)
@@ -32,12 +31,55 @@ const handleClick = (element) => {
   if (!document.getElementById(element.id).innerHTML) {
     addMarker(element.id)
   }
+
+  checkForWin()
+
 };
 
 
 
 
+const checkForWin = () => {
+  if (horizontalWin() || verticalWin() || diagonalWin()) {
+    setTimeout(() => {
+      window.alert(`Player ${currentMarker} won!`);
+    }, 100);
+  } else {
+    changeMarker()
+  }
+}
 
+const horizontalWin = () => {
+  if (
+    (board[0][0] !== null && board[0][0] === board[0][1] && board[0][0] === board[0][2]) ||
+    (board[1][0] !== null && board[1][0] === board[1][1] && board[1][0] === board[1][2]) ||
+    (board[2][0] !== null && board[2][0] === board[2][1] && board[2][0] === board[2][2])
+  ) {
+    return true
+  }
+  return false
+}
+
+const verticalWin = () => {
+  if (
+    (board[0][0] !== null && board[0][0] === board[1][0] && board[0][0] === board[2][0]) ||
+    (board[0][1] !== null && board[0][1] === board[1][1] && board[0][1] === board[2][1]) ||
+    (board[0][2] !== null && board[0][2] === board[1][2] && board[0][2] === board[2][2])
+  ) {
+    return true
+  }
+  return false
+}
+
+const diagonalWin = () => {
+  if (
+    (board[0][0] !== null && board[0][0] === board[1][1] && board[0][0] === board[2][2]) ||
+    (board[2][0] !== null && board[2][0] === board[1][1] && board[2][0] === board[0][2])
+  ) {
+    return true
+  }
+  return false
+}
 
 
 
@@ -60,12 +102,11 @@ const addMarker = (id) => {
 
   const values = id.split('-');
   const row = parseInt(values[0]);
-  const column = parseInt(values[0]);
+  const column = parseInt(values[1]);
 
   board[row][column] = currentMarker;
   console.log('board', board[row][column], board)
   document.getElementById(id).innerHTML = currentMarker;
-  changeMarker()
 };
 
 const changeMarker = () => {
@@ -80,12 +121,14 @@ const resetBoard = () => {
   const squares = document.getElementsByTagName("td");
   for (i = 0; i < squares.length; i++) {
     console.log(squares[i].id)
-    squares[i].innerHTML = null
+    squares[i].innerHTML = '';
+    board = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null]
+    ];
   }
 };
 
 // start TTT-Logic
 
-if (board[0][0] == board[0][1] && board[0][0] == board[0][2]) {
-
-}
